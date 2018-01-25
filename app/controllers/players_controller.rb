1e6afd302aabbedb5e995f59347ea9fe58bbee8c
Ronald Walker <rondwalker22@gmail.com>
2018-01-25 13:33:33 -0800

    added validation to sign up and edit player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,3 +1,4 @@
+# require 'pry'
 class PlayersController < ApplicationController
   before_action :set_player, only: [:show, :edit, :update, :destroy]
 
@@ -25,14 +26,16 @@ class PlayersController < ApplicationController
   # POST /players.json
   def create
     @player = Player.new(player_params)
-
     respond_to do |format|
-      if @player.save
-        format.html { redirect_to @player, notice: 'Player was successfully created.' }
+      if valid_player? &&  @player.save
+        flash[:notice] = 'Player was successfully created.'
+        format.html { redirect_to @player}
         format.json { render :show, status: :created, location: @player }
       else
+        flash.now[:notice] = 'Player not created. All fields are required.'
         format.html { render :new }
-        format.json { render json: @player.errors, status: :unprocessable_entity }
+        format.json { render json: @player.errors, status:
+         :unprocessable_entity }
       end
     end
   end
@@ -41,10 +44,12 @@ class PlayersController < ApplicationController
   # PATCH/PUT /players/1.json
   def update
     respond_to do |format|
-      if @player.update(player_params)
-        format.html { redirect_to @player, notice: 'Player was successfully updated.' }
+      if valid_player? && @player.update(player_params)
+        flash[:notice] = 'Player was successfully updated.'
+        format.html { redirect_to @player}
         format.json { render :show, status: :ok, location: @player }
       else
+        flash.now[:notice] = 'Update failed. All fields are required.'
         format.html { render :edit }
         format.json { render json: @player.errors, status: :unprocessable_entity }
       end
@@ -56,7 +61,8 @@ class PlayersController < ApplicationController
   def destroy
     @player.destroy
     respond_to do |format|
-      format.html { redirect_to players_url, notice: 'Player was successfully destroyed.' }
+      flash[:notice] = 'Player was successfully destroyed.'
+      format.html { redirect_to players_url}
       format.json { head :no_content }
     end
   end
@@ -65,6 +71,11 @@ class PlayersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_player
       @player = Player.find(params[:id])
+    end
+
+    def valid_player?
+      !@player.username.empty? && !@player.email.empty? &&
+        !@player.password.empty?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

95058bad7469f4045cf700e7564624f42af4fab4
Ronald Walker <rondwalker22@gmail.com>
2018-02-11 14:59:40 -0800

    added login tests

---
+++ test/integration/players_login_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class PlayersLoginTest < ActionDispatch::IntegrationTest
+  test "login with invalid information"do 
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "1238"} } 
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information"do 
+    def setup
+      @player = players('sam')
+    end
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "sam@gmail.com", 
+                                          password: "123"} }
+    assert_redirected_to leaderboards_path
+    follow_redirect! 
+    assert_template 'players/index'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+  end
+end

846f348240c469a5f5834e23648541b5c80fe422
Ronald Walker <rondwalker22@gmail.com>
2018-05-06 15:35:00 -0700

    tiny edit

--- config/routes.rb
+++ config/routes.rb
@@ -35,6 +35,6 @@ Rails.application.routes.draw do
   # get 'profile' => 'players#'
   # get 'profile/edit' => 'players#'
 
-  mount ActionCable.server, at: '/cable'
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

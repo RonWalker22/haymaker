10a41a62bfc781a562f27d18c7a80ea2d0de1941
Ronald Walker <rondwalker22@gmail.com>
2018-09-17 11:07:20 -0700

    Validate no White Space in User Name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates_uniqueness_of :name, case_sensitive: false
+  validates :name, format: { with: /\A[a-zA-Z0-9]+\Z/,
+            message: "must contain only letters and numbers. No spaces." }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :league_users,                                 dependent: :delete_all
